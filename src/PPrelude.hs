dabf2a1540830179f5ed7187b7567e2cd1ee70ac
fanghr <chfanghr@gmail.com>
2022-04-06 17:18:20 +0800

    use plutarch's prelude as prelude

---
+++ src/PPrelude.hs
@@ -0,0 +1,9 @@
+module PPrelude (
+  module Prelude,
+  module Plutarch.Prelude,
+  module Plutarch,
+) where
+
+import Plutarch (ClosedTerm, POpaque, compile)
+import Plutarch.Prelude
+import Prelude
---
+++ src/Stuff/Dummy.hs
@@ -0,0 +1,1 @@
+module Stuff.Dummy() where
